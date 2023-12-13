a836d1432fb5e8be0275415def909386010ccaf6
Victor W Allen <vwallen@antlab.net>
2023-12-13 08:35:08 -0800

    Day 12 - More optimization, part 2 prep

--- src/day12.rs
+++ src/day12.rs
@@ -15,44 +15,49 @@ impl Record {
                 '.'|'#' => options.iter().map(|opt| opt.clone() + &ch.to_string()).collect(),
                 '?' => {
                     options.iter().fold(vec![], |mut opts, opt| {
-                        opts.push(opt.clone() + &".");
-                        opts.push(opt.clone() + &"#");
+                        if let Some(a) = self.check_partial(opt.clone() + &".") {
+                            opts.push(a);
+                        }
+                        if let Some(b) = self.check_partial(opt.clone() + &"#") {
+                            opts.push(b);
+                        }
                         opts
                     })
                 },
                 _ => unreachable!(),
             }
-            .iter()
-            .filter(|opt| self.check_partial(opt))
-            .map(String::clone)
-            .collect();
         }
         options
     }
 
-    pub fn check_partial(&self, permutation:&String) -> bool {
+    pub fn check_partial(&self, permutation:String) -> Option<String> {
         let sequence:Vec<usize> = permutation
             .split(".")
-            .map(|s| s.len())
-            .filter(|s| s > &0_usize)
+            .filter_map(|s| if s.is_empty() { None } else { Some(s.len())})
             .collect();
         for (i, block) in sequence.iter().enumerate() {
             if let Some(requirement) = self.sequence.get(i) {
                 if block > &requirement {
-                    return false
+                    return None
                 }
             }
         }
-        true
+        Some(permutation.clone())
     }
 
     pub fn check_permutation(&self, permutation:&String) -> bool {
         let sequence:Vec<usize> = permutation
             .split(".")
-            .map(|s| s.len())
-            .filter(|s| s > &0_usize)
+            .filter_map(|s| if s.is_empty() { None } else { Some(s.len())})
             .collect();
         sequence == self.sequence
+    }
+
+    pub fn to_long(&self) -> Record {
+        Record {
+            pattern:  vec![self.pattern.clone(); 5].join("?"),
+            sequence: vec![self.sequence.clone(); 5].concat(),
+        }
     }
 }
 
@@ -63,7 +68,7 @@ pub fn prepare(file_name: &str) -> Result<Vec<Record>> {
         .map(|line| {
             let (pattern, sequence) = line.split_once(" ").unwrap();
             Record{
-                pattern: pattern.to_string(),
+                pattern:  pattern.to_string(),
                 sequence: sequence.split(",").map(|s| s.parse().unwrap()).collect(),
             }
         })
@@ -73,18 +78,31 @@ pub fn prepare(file_name: &str) -> Result<Vec<Record>> {
 
 pub fn part_1(input: &Vec<Record>) -> Option<usize> {
     let mut count = 0;
-    for record in input.iter() {
-        count += record
+    for (_i, record) in input.iter().enumerate() {
+        let valid = record
             .collect_permutations()
             .iter()
-            .filter(|perm| record.check_permutation(&perm))
+            .filter(|perm| record.check_permutation(perm))
             .count();
+        count += valid;
+        // println!("{i}: {:-<25}> {valid: >2}", record.pattern);
     }
     Some(count)
 }
 
-pub fn part_2(_input: &Vec<Record>) -> Option<usize> {
-    None
+pub fn part_2(input: &Vec<Record>) -> Option<usize> {
+    let mut count = 0;
+    for (_i, record) in input.iter().enumerate() {
+        let long_record = record.to_long();
+        let valid = long_record
+            .collect_permutations()
+            .iter()
+            .filter(|perm| long_record.check_permutation(perm))
+            .count();
+        count += valid;
+        // println!("{i}: {:?} -> {valid}", long_record.pattern);
+    }
+    Some(count)
 }
 
 #[cfg(test)]
@@ -100,6 +118,7 @@ mod test {
     }
 
     #[test]
+    #[ignore]
     fn test_part_1_puzzle() {
         if let Ok(input) = prepare("day12.txt") {
             assert_eq!(part_1(&input), Some(7771))
@@ -110,7 +129,7 @@ mod test {
     #[ignore]
     fn test_part_2() {
         if let Ok(input) = prepare("day12-example.txt") {
-            assert_eq!(part_2(&input), None)
+            assert_eq!(part_2(&input), Some(525152))
         }
     }
 }
